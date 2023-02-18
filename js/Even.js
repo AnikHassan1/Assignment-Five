a13ee656d2527887a6923916158f2a8c824a3204
Anik Hassan1 <ah491606@gmail.com>
2023-02-18 13:38:37 +0600

    bg-Change

--- js/Even.js
+++ js/Even.js
@@ -1,36 +1,8 @@
-// document.getElementById('btn-blog').addEventListener('click',function(){
-//     window.location='second.html';
-// })
+document.getElementById('btn-first').addEventListener('click',function(){
+    window.location .href='Secound.html';
+})
 
-//  Using By Commond function  of Input Value.
-function common(id) {
-    const commonInput = document.getElementById(id);
-    const commonString = commonInput.value;
-    const commonValue = parseInt(commonString);
-    commonInput.value='';
-    return commonValue;
-}
-//  Using By Commond function  of innertext.
-function text(text) {
-    const Text = document.getElementById(text);
-    const TextValue = Text.innerText;
-    return TextValue;
-}
-// creatre Element By Html File .
-function displayData(Name, total) {
-    const container = document.getElementById("table-container");
-    const tr = document.createElement("tr");
-    tr.innerHTML = `
-      <td>${serial}</td>
-      <td>${Name}</td>
-      <td>${total}cm <sup>2</sup></td>
-      <td>
-      <button class="btn btn-sm btn-red-500  lg:w-full button">Convert To m <sup>2</sup> </button>
-      </td>
-    `;
-    container.appendChild(tr);
 
-}
 
 //   Start Dom Fuction use The Project.
 let serial = 0;
@@ -38,12 +10,16 @@ document.getElementById('btn1').addEventListener('click', function () {
     serial += 1;
     const name = text('text');
     const tri = common('tri');
+  
     const tri2 = common('tri2');
+  
     const triTotal = 0.5 * tri * tri2;
     const value = triTotal.toFixed(2);
     displayData(name, value);
+
 })
 // rec
+
 document.getElementById('btn-rec').addEventListener('click', function () {
     serial += 1;
     const name = text('rec');
@@ -103,7 +79,38 @@ document.getElementById('btn-ELi').addEventListener('click', function () {
     const tri2 = common('eli2');
     const triTotal = 3.1416 * tri * tri2;
     const value = triTotal.toFixed(2);
+
+    
     displayData(name, value);
 })
 
+//  Using By Commond function  of Input Value.
+function common(id) {
+    const commonInput = document.getElementById(id);
+    const commonString = commonInput.value;
+    const commonValue = parseInt(commonString);
+    commonInput.value='';
+    return commonValue;
+}
 
+//  Using By Commond function  of innertext.
+function text(text) {
+    const Text = document.getElementById(text);
+    const TextValue = Text.innerText;
+    return TextValue;
+}
+// creatre Element By Html File .
+function displayData(Name, total) {
+    const container = document.getElementById("table-container");
+    const tr = document.createElement("tr");
+    tr.innerHTML = `
+      <td>${serial}</td>
+      <td>${Name}</td>
+      <td>${total}cm <sup>2</sup></td>
+      <td>
+      <button id="btn-con" class="btn btn-sm btn-red-500  lg:w-full button">Convert To m <sup>2</sup> </button>
+      </td>
+    `;
+    container.appendChild(tr);
+
+}
